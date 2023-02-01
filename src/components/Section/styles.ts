99d488bb1ed54c51855ace434d4fd9e21fa50980
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2023-02-01 12:36:42 -0300

    Better padding for mobile

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -5,4 +5,10 @@ export const Container = styled.div`
   justify-content: center;
 `;
 
-export const Content = styled.section``;
+export const Content = styled.section`
+  padding: 0 2rem;
+
+  @media (min-width: 1100px) {
+    padding: 0;
+  }
+`;
