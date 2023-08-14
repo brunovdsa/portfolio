4ef26301d6bc095202a4a676f533d0e72321c1bf
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-08-14 14:35:56 -0300

    FIX: Photo fixed.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,7 +5,7 @@ import Card from '../../components/BlurCard';
 import Section from '../../components/Section';
 import { MainContainer } from '../../components/MainContainer';
 
-import me from '../../assets/img/me.webp';
+import me from '../../assets/img/me2.webp';
 
 import {
   AboutContainer,
