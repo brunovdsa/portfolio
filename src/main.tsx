d109293ba678862fdff28774a523cf3ecfa6534b
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-08-24 09:07:18 -0300

    FIX: Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics/*';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Analytics />
     <App />
   </React.StrictMode>
 );
