07c8a427dd694e1e590ed7d553f5315e6bc5e89f
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-08-24 09:05:35 -0300

    TEST: Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Router from './routes/Router';
-import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
-      <Analytics />
       <Router />
     </>
   );
