afaef3579ed2a588f36bfbb010519925784e0bae
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-08-24 09:08:51 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { inject } from '@vercel/analytics/';
-
-inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
