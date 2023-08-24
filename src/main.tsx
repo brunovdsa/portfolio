a854bb9ccfcba51b54a55dc00750563b4c1cdd71
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-08-24 09:08:10 -0300

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { inject } from '@vercel/analytics/*';
+import { inject } from '@vercel/analytics/';
 
 inject();
 
